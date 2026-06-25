c7eebdb050c90bdcbbaeb60c2344c1f2ec11af45
Alexandre Tranchant <alexandre.tranchant@gmail.com>
2026-06-25 10:15:31 +0200

    Headers updated

--- lib/LongitudeOne/Geo/WKT/Exception/NotInstantiableException.php
+++ lib/LongitudeOne/Geo/WKT/Exception/NotInstantiableException.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the LongitudeOne WKT-Parser project.
  *
- * PHP 8.1 | 8.2 | 8.3
+ * PHP 8.1 - 8.5
  *
  * Copyright LongitudeOne - Alexandre Tranchant - Derek J. Lambert.
  * Copyright 2024-2026.
